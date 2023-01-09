c3fb7c45bbb0d42c1811d17310b55018b332a5c4
seohyun56 <iw000125@naver.com>
2023-01-09 15:14:12 +0900

    youtube

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Checkbox from "./components/Checkbox";
+import Youtube1 from "./components/Youtube1";
+import Youtube2 from "./components/Youtube2";
 
 function App() {
   return (
     <>
-      <Checkbox />
+      {/* <Checkbox /> */}
+      {/* <Youtube1 /> */}
+      <Youtube2 />
     </>
   );
 }
